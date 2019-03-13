880c86ecabe009eb9246b868ca701ae8b9c5225f
NMilho <nuno.milho@gmail.com>
2019-03-13 00:23:16 +0000

    navbar.inc testes

--- functions.php
+++ functions.php
@@ -25,4 +25,20 @@ function mdbtheme_setup() {
     add_theme_support('post-thumbnails');
 }
 add_action('after_setup_theme', 'mdbtheme_setup');
+
+
+
+/**
+ * Register Menus
+ */
+function register_my_menus() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'extra-menu' => __( 'Extra Menu' )
+     )
+   );
+ }
+ add_action( 'init', 'register_my_menus' );
+
 ?>
--- header.php
+++ header.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
     <!-- Required meta tags always come first -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="description" content="">
+	<meta name="author" content="">
     <title>
         <?php bloginfo( 'name'); ?>
     </title>
     <?php wp_head(); ?>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
