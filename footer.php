0c534008f838035e77012a1a80d17ae2956a9a44
nmilho <nuno.milho@gmail.com>
2019-03-14 16:41:38 +0000

    [WIP] Footer

--- footer.php
+++ footer.php
@@ -1,3 +1,52 @@
-<?php wp_footer(); ?>
+
+<footer class="page-footer text-center font-small mdb-color darken-2 mt-4 wow fadeIn" style="visibility: visible; animation-name: fadeIn;">
+
+    <hr class="my-4">
+
+    <!-- Social icons -->
+    <div class="pb-4">
+      <a href="https://www.facebook.com/mdbootstrap" target="_blank">
+        <i class="fa fa-facebook-f mr-3"></i>
+      </a>
+
+      <a href="https://twitter.com/MDBootstrap" target="_blank">
+        <i class="fa fa-twitter mr-3"></i>
+      </a>
+
+      <a href="https://www.youtube.com/watch?v=7MUISDJ5ZZ4" target="_blank">
+        <i class="fa fa-youtube mr-3"></i>
+      </a>
+
+      <a href="https://plus.google.com/u/0/b/107863090883699620484" target="_blank">
+        <i class="fa fa-google-plus-g mr-3"></i>
+      </a>
+
+      <a href="https://dribbble.com/mdbootstrap" target="_blank">
+        <i class="fa fa-dribbble mr-3"></i>
+      </a>
+
+      <a href="https://pinterest.com/mdbootstrap" target="_blank">
+        <i class="fa fa-pinterest mr-3"></i>
+      </a>
+
+      <a href="https://github.com/mdbootstrap/bootstrap-material-design" target="_blank">
+        <i class="fa fa-github mr-3"></i>
+      </a>
+
+      <a href="http://codepen.io/mdbootstrap/" target="_blank">
+        <i class="fa fa-codepen mr-3"></i>
+      </a>
+    </div>
+    <!-- Social icons -->
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3">
+      © <?php echo date("Y"); ?> Copyright:
+      <a href="https://aphh/" target="_blank"> APHH.pt </a>
+    </div>
+    <!--/.Copyright-->
+
+  </footer>
+  <?php wp_footer(); ?>
 </body>
 </html>
